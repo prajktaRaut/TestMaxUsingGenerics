535319b9922e170077f4001869313da8459e12c3
prajktaRaut <prajktasraut16@gmail.com>
2019-11-29 15:26:09 +0530

    Find the Float max value at First position and return it

--- src/main/java/com/maximumvalue/MaximumValue.java
+++ src/main/java/com/maximumvalue/MaximumValue.java
@@ -2,7 +2,7 @@ package com.maximumvalue;
 
 public class MaximumValue {
 
-    public Integer findMaxAtFirstPosition(Integer i, Integer j, Integer k) {
+    public Integer findMaxAccordingToPosition(Integer i, Integer j, Integer k) {
 
         if (i.compareTo(j) > 0)
             return i;
@@ -12,6 +12,19 @@ public class MaximumValue {
             return k;
 
         return null;
+    }
+
+    public Float findMaxAccordingToPosition(Float i, Float j, Float k) {
+
+        if (i.compareTo(j) > 0)
+            return i;
+        if (j.compareTo(k) > 0)
+            return j;
+        if (k.compareTo(i) > 0)
+            return k;
+
+        return null;
+
     }
 }
 
--- src/test/java/com/testmax/MaximumValueTest.java
+++ src/test/java/com/testmax/MaximumValueTest.java
@@ -7,31 +7,38 @@ import org.junit.Test;
 public class MaximumValueTest {
 
     @Test
-    public void givenMethod_FindMaxValue_PresentAtFirstPositionAndReturn() {
+    public void givenMethod_FindIntegerMaxValue_PresentAtFirstPositionAndReturn() {
 
         MaximumValue maximumValue=new MaximumValue();
-        Integer max=maximumValue.findMaxAtFirstPosition(76,34,45);
+        Integer max=maximumValue.findMaxAccordingToPosition(76,34,45);
         Assert.assertEquals(Integer.valueOf(76),max);
 
     }
 
     @Test
-    public void givenMethod_FindMaxValue_PresentAtSecondPositionAndReturn() {
+    public void givenMethod_FindIntegerMaxValue_PresentAtSecondPositionAndReturn() {
 
         MaximumValue maximumValue=new MaximumValue();
-        Integer max=maximumValue.findMaxAtFirstPosition(34,76,45);
+        Integer max=maximumValue.findMaxAccordingToPosition(34,76,45);
         Assert.assertEquals(Integer.valueOf(76),max);
 
     }
 
     @Test
-    public void givenMethod_FindMaxValue_PresentAtThirdPositionAndReturn() {
+    public void givenMethod_FindIntegerMaxValue_PresentAtThirdPositionAndReturn() {
 
         MaximumValue maximumValue=new MaximumValue();
-        Integer max=maximumValue.findMaxAtFirstPosition(34,45,76);
+        Integer max=maximumValue.findMaxAccordingToPosition(34,45,76);
         Assert.assertEquals(Integer.valueOf(76),max);
 
     }
 
+    @Test
+    public void givenMethod_FindFloatMaxValue_PresentAtFirstPositionAndReturn() {
 
+        MaximumValue maximumValue=new MaximumValue();
+        Float max=maximumValue.findMaxAccordingToPosition(7.6f,3.4f,4.5f);
+        Assert.assertEquals(Float.valueOf(7.6f),max);
+
+    }
 }
